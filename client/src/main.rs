14f95e659561f2af1615613c17af001ccfc38121
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-06 15:59:54 -0700

    Add args and ignore

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Command {
+    Chat {},
+}
+
+fn parse_args() {
+    match Command::from_args() {
+        Command::Chat {} => {
+            println!("Chat!");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    parse_args();
 }
