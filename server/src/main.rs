407801b8890516416ddc7ca98cd0fe4d3b5f3536
adamhutchings <adam.abahot@gmail.com>
2022-08-06 23:14:41 -0700

    Add test notification

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
+use notify_rust::Notification;
+
 fn main() {
+
     println!("Hello, world!");
+
+    Notification::new()
+    .summary("RCLC")
+    .body("The RCLC server has been launched!")
+    .show();
+
 }
