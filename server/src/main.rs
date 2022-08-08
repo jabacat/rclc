6d407cf1347c345d45805bea08d522382616c790
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-07 21:40:44 -0700

    Add Rocket

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, server!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn home() -> String {
+    "Hello Jabacat!".to_string()
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![home])
 }
