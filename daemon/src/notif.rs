fa0af170468d0518151e566e1ec6c698a859bc8c
adamhutchings <adam.abahot@gmail.com>
2022-08-09 11:54:43 -0700

    Ascend at Rust

--- daemon/src/notif.rs
+++ daemon/src/notif.rs
@@ -1,15 +1,16 @@
 use notify_rust::Notification;
 
-/**
- * This is just a thin wrapper around the Notification stuff for now.
- * The library says it always returns Ok, so we're not worrying about anything
- * else at the moment.
- */
+/// This is just a thin wrapper around the Notification stuff for now.
+/// The library says it always returns Ok, so we're not worrying about anything
+/// else at the moment.
 pub fn notif(summary: &str, msg: &str) {
-    match Notification::new().summary(summary).body(msg).show() {
-        Ok(_) => (),
-        // This should never happen. TODO: log this event which supposedly
-        // never happens.
-        Err(_) => eprintln!("Unable to display notification!"),
-    }
+    Notification::new()
+        .summary(summary)
+        .body(msg)
+        .show()
+        .expect(
+            // This should never happen. TODO: log this event which supposedly
+            // never happens.
+            "Unable to display notification!",
+        );
 }
