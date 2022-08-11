4f937518faa349913fcb2d2e092ca7613a63b9e5
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-08-10 19:26:14 -0700

    Add new route for discover root

--- daemon/src/discovery.rs
+++ daemon/src/discovery.rs
@@ -5,10 +5,21 @@ pub struct DiscoveryServerConfig {
     pub url: String,
 }
 
+async fn discover_root(disc_conf: DiscoveryServerConfig) {
+    let client = reqwest::Client::new();
+    let res = client
+        .get(disc_conf.url)
+        .send()
+        .await
+        .expect("Could not await");
+
+    println!("res = {:?}", res);
+}
+
 async fn discover(disc_conf: DiscoveryServerConfig, disc_request: DiscoveryRequest) {
     let client = reqwest::Client::new();
     let res = client
-        .post(disc_conf.url)
+        .post(disc_conf.url + "/discover")
         .body("Hello!")
         .send()
         .await
