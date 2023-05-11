38e5473c6a0f8a6074928331d0f69959dbc298f0
Camerooooon <cjpuffnstuff@gmail.com>
2023-05-11 00:11:06 -0700

    Add setup for testing queue cleaning

--- server/src/cleanable.rs
+++ server/src/cleanable.rs
@@ -23,6 +23,7 @@ impl Cleanable for DiscoveryQueue {
                             // Ok signifies that the duration since the specified time was
                             // positive, therefor the advertisement is expired
                             to_remove.push(ele.0.to_string());
+                            println!("Removing request by {}", ele.1.discovery.requested_by);
                         }
                         Err(_) => {
                             // The advertisement is not expired yet
@@ -40,5 +41,45 @@ impl Cleanable for DiscoveryQueue {
                 .remove(ele);
         }
         Ok(to_remove.len())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        collections::HashMap,
+        net::{IpAddr, Ipv4Addr},
+        sync::RwLock,
+        time::{Duration, SystemTime},
+    };
+
+    use common::structures::DiscoveryRequest;
+
+    use crate::discovery::{Advertisement, DiscoveryQueue};
+
+    #[test]
+    fn clean_test() {
+        let discoveryqueue = DiscoveryQueue {
+            queue: RwLock::new(HashMap::new()),
+        };
+
+        let mut locked_queue = discoveryqueue.queue.write().unwrap();
+
+        let disc_req_a = DiscoveryRequest {
+            ip: Some(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1))),
+            port: 2121,
+            requested_by: "A".to_string(),
+            looking_for: "B".to_string(),
+            public_key: "qwertyuiop".to_string(),
+        };
+
+        locked_queue.insert(
+            "A".to_string(),
+            Advertisement {
+                discovery: disc_req_a,
+                created_at: SystemTime::now(),
+                expires_in: Duration::from_millis(100),
+            },
+        );
     }
 }
