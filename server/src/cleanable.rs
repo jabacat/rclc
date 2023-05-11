c06ded3fc07cbe67ecb214ef580bf3a92e37fd5a
Camerooooon <cjpuffnstuff@gmail.com>
2023-05-10 23:58:03 -0700

    Add logic for removing stale advertisements

--- server/src/cleanable.rs
+++ server/src/cleanable.rs
@@ -1,4 +1,4 @@
-use std::time::{Duration, SystemTime};
+use std::time::SystemTime;
 
 use crate::discovery::DiscoveryQueue;
 
@@ -13,14 +13,31 @@ pub trait Cleanable {
 
 impl Cleanable for DiscoveryQueue {
     fn clean(&self) -> Result<u16, CleanError> {
-        match self.queue.try_read().map_err(|e| CleanError::LockError) {
+        let mut to_remove: Vec<String> = vec![];
+        match self.queue.try_read().map_err(|_| CleanError::LockError) {
             Ok(it) => {
-                for ele in it.values() {
-                    SystemTime::now().checked_add(ele.expires_in);
+                for ele in it.iter() {
+                    match SystemTime::now().duration_since(ele.1.created_at + ele.1.expires_in) {
+                        Ok(_) => {
+                            // Ok signifies that the duration since the specified time was
+                            // positive, therefor the advertisement is expired
+                            to_remove.push(ele.0.to_string());
+                        }
+                        Err(_) => {
+                            // The advertisement is not expired yet
+                        }
+                    }
                 }
             }
             Err(err) => return Err(err),
         };
-        Ok(0)
+
+        for ele in &to_remove {
+            self.queue.write().expect("").remove(ele);
+        }
+        Ok(to_remove
+            .len()
+            .try_into()
+            .expect("Length of removed items was not expected to be so long"))
     }
 }
