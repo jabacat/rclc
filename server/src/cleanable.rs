690258c49b6dec92655d076a4010b82eb3e29259
Camerooooon <cjpuffnstuff@gmail.com>
2023-05-10 23:37:51 -0700

    Create cleanable trait

---
+++ server/src/cleanable.rs
@@ -0,0 +1,26 @@
+use std::time::{Duration, SystemTime};
+
+use crate::discovery::DiscoveryQueue;
+
+pub enum CleanError {
+    LockError,
+}
+
+pub trait Cleanable {
+    // Returns the amount of instances that have been cleaned from the queue
+    fn clean(&self) -> Result<u16, CleanError>;
+}
+
+impl Cleanable for DiscoveryQueue {
+    fn clean(&self) -> Result<u16, CleanError> {
+        match self.queue.try_read().map_err(|e| CleanError::LockError) {
+            Ok(it) => {
+                for ele in it.values() {
+                    SystemTime::now().checked_add(Duration::from_millis(ele.expires_in));
+                }
+            }
+            Err(err) => return Err(err),
+        };
+        Ok(0)
+    }
+}
