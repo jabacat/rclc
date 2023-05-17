8460ae3adb3a59b45c8df754788d830a2cff8ae6
Camerooooon <cjpuffnstuff@gmail.com>
2023-05-16 19:22:26 -0700

    Add comments

--- common/src/client_daemon.rs
+++ common/src/client_daemon.rs
@@ -55,23 +55,34 @@ pub enum DaemonToClientMsg {
     Unknown,
 }
 
-pub fn parse_message(data: Vec<u8>) -> Result<ClientToDaemonMsg, PacketParseError> {
+/// Parses a Vec<u8> into a ClientToDaemonMsg object
+pub fn parse_message_client_to_daemon(
+    data: Vec<u8>,
+) -> Result<ClientToDaemonMsg, PacketParseError> {
     let out: ClientToDaemonMsg = postcard::from_bytes(&data).map_err(|_| PacketParseError)?;
     Ok(out)
 }
 
+/// Converts a DaemonToClientMsg object into bytes to be sent over a stream
 pub fn serialize_message_daemon_to_client(
     message: DaemonToClientMsg,
 ) -> Result<Vec<u8>, postcard::Error> {
     //let out: heapless::Vec<u8, 128> = postcard::to_vec(&message)?;
     let out: Vec<u8> = postcard::to_vec::<DaemonToClientMsg, 16384>(&message)?.to_vec();
+    // TODO: The number 16384 is the amount of bytes that will be read, if it exceeds this number it will
+    // error. I'm not yet sure how to make this dyanmically alocate so I just set it to a really
+    // high multiple of 2.
     Ok(out.to_vec())
 }
 
+/// Converts a ClientToDaemon object into bytes to be sent over a stream
 pub fn serialize_message_client_to_daemon(
     message: ClientToDaemonMsg,
 ) -> Result<Vec<u8>, postcard::Error> {
     //let out: heapless::Vec<u8, 128> = postcard::to_vec(&message)?;
     let out: Vec<u8> = postcard::to_vec::<ClientToDaemonMsg, 16384>(&message)?.to_vec();
+    // TODO: The number 16384 is the amount of bytes that will be read, if it exceeds this number it will
+    // error. I'm not yet sure how to make this dyanmically alocate so I just set it to a really
+    // high multiple of 2.
     Ok(out.to_vec())
 }
